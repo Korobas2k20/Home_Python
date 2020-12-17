4eb155e6ef3ccb131d1cfb1f097e5027f7a075a7
Korobas2k20 <62026527+Korobas2k20@users.noreply.github.com>
2020-12-18 00:44:27 +0500

    dz

--- home1/work.py
+++ home1/work.py
@@ -1,18 +1,14 @@
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
-c = str(input("Выберите действие(+-/*): "))
+a = input("Введите имя файла: ")
 
-if c == "+":
-    a = a + b
-    print("Ваше число:",a)
-elif c == "-":
-    a = a - b
-    print("Ваше число:",a)
-elif c == "/":
-    a = a / b
-    print("Ваше число:",a)
-elif c == "*":
-    a = a * b
-    print("Ваше число:",a)
+if a.endswith(".html") or a.endswith(".htm") or a.endswith(".php"): #C:\DOC\САЙТ\index.html
+    print("Это веб-страница!")
+elif a.endswith(".doc") or a.endswith(".docx"):
+    print("Это документ Word!")
+elif a.endswith(".xls") or a.endswith(".xlsx"):
+    print("Это документ Excel!")
+elif a.endswith(".zip") or a.endswith(".rar") or a.endswith(".7z"):
+    print("Это архив!")
+elif a.endswith(".exe"):
+    print("Это программа!")
 else:
-    print("Я не знаю эту операцию!")
+    print("Это что-то вообще другое!")
